b54042dc47d9f4f4a4d5345b00f3613dd0d9ec7b
илья Скибиди <lastochka1230@yandex.ru>
2025-03-07 08:56:07 +0500

    Реализованный метод ParsingHtml. Суханова.

--- ParsingHtml/Program.cs
+++ ParsingHtml/Program.cs
@@ -5,13 +5,17 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace ParsingHtml
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var content = await GetContent();
+            var res = ParsingHtml(content);
+            Console.WriteLine(res);
         }
         public static async Task<string> GetContent()
         {
@@ -21,6 +25,29 @@ namespace ParsingHtml
             string FinalStream = new StreamReader(response.GetResponseStream()).ReadToEnd();
             return FinalStream;
         }
-        
+        public static string ParsingHtml(string htmlContent)
+        {
+            var html = new HtmlDocument();
+            html.LoadHtml(htmlContent);
+            var document = html.DocumentNode;
+            string answer = "";
+            var newsItems = document.Descendants("div").Where(n => n.HasClass("news"));
+            foreach ( var item in newsItems )
+            {
+                var headers = item.Descendants("div").Where(n => n.HasClass("header"));
+                var texts = item.Descendants("div").Where(n => n.HasClass("text"));
+                foreach (var header in headers)
+                {
+                    foreach ( var text in texts)
+                    {
+                        var descriptionHeader = header.InnerHtml.Trim();
+                        var description = text.InnerText.Trim();
+                        answer += $"Событие: {descriptionHeader}\n{description}\n";
+                        answer += new string('-', 40) + "\n";
+                    }
+                }
+            }
+            return answer;
+        }
     } 
 }
