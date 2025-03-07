6593bd794d14be1a4e6333cbd8727243b07a7015
илья Скибиди <lastochka1230@yandex.ru>
2025-03-07 08:48:52 +0500

    Создан метод GetContent(). Суханова

--- ParsingHtml/Program.cs
+++ ParsingHtml/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,14 @@ namespace ParsingHtml
     {
         static void Main(string[] args)
         {
+        }
+        public static async Task<string> GetContent()
+        {
+            string url = "https://phtt.ru/news/2024/";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string FinalStream = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            return FinalStream;
         }
         
     } 
